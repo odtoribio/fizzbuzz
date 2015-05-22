d63c2dc5a06b0b4995b02cb6db5be3b21020dcff
Oscar Davis <odtoribio@gmail.com>
2015-05-22 13:02:36 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import jinja2
+import webapp2
+
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+class Handler(webapp2.RequestHandler):
+
+    def write(self,*a,**kw):
+        self.response.write(*a,**kw)
+
+    def render_str(self,template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self,template,**kw):
+        self.write(self.render_str(template,**kw))
+
+class MainPage(Handler):
+
+    def get(self):
+        n = self.request.get("n")
+        if n:
+            n = int(n)
+        self.render('fizzbuzz.html', n= n)
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
